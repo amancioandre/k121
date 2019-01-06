f13711a02470a50a8b3ca0039bd34d658a320229
Andre Moraes <amancioandre@gmail.com>
2019-01-06 18:58:39 -0200

    Updates

---
+++ precache-manifest.24d39b6e714a4938f51851c6409ea2cc.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "175395750514f9ace358",
+    "url": "/k121/static/js/runtime~main.17539575.js"
+  },
+  {
+    "revision": "913023ab1e23d0babb7a",
+    "url": "/k121/static/js/main.913023ab.chunk.js"
+  },
+  {
+    "revision": "2e70065872dad3ae0fae",
+    "url": "/k121/static/js/1.2e700658.chunk.js"
+  },
+  {
+    "revision": "913023ab1e23d0babb7a",
+    "url": "/k121/static/css/main.01a45ea0.chunk.css"
+  },
+  {
+    "revision": "022bd6db5a687324c546f8268c4043b9",
+    "url": "/k121/index.html"
+  }
+];
